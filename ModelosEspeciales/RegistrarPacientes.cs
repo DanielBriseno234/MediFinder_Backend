ebe50424d1ee376c7597add850527c281269b0e3
Jessy <jg.delgadochavez@gmail.com>
2024-07-15 00:02:26 -0600

    Se termina metodos medicos

---
+++ ModelosEspeciales/RegistrarPacientes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediFinder_Backend.ModelosEspeciales
+{
+    public class RegistrarPacientes
+    {
+        public class LoginDTO
+        {
+            [Required(ErrorMessage = "El campo Email es requerido")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "El campo Contraseña es requerido")]
+            public string Contrasena { get; set; }
+        }
+    }
+}
